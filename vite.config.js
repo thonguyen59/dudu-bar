39c37abfdf0ee03ba6ccef4cdf17dfdc32725b9e
ThoNguyen <thonguyen0110.work@gmail.com>
2025-05-27 16:38:52 +0700

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/dudu-bar/', // VD: /vu-bar/
+  base: '/dudu-bar',
   plugins: [react(), tailwindcss()],
 })
